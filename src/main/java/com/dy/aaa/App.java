db8008433aeaf596d77e3e0ed46861d5c0d60c9a
tom_glb <goodmorning_glb@qq/com>
2019-06-11 22:36:19 +0800

    ss

---
+++ src/main/java/com/dy/aaa/App.java
@@ -0,0 +1,13 @@
+package com.dy.aaa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
